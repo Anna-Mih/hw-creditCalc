4b53cbbd7eb5317b602cb62baa313f45218d499c
anna-mih <anedbalskaya@yandex.ru>
2022-03-27 13:20:52 +0300

    reformat code in .java files

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,18 +1,9 @@
 public class CreditPaymentService {
     public double calculate(double creditSum, double creditPeriodYear) {
-
         double creditRateMonth = 9.99 / 12 / 100;
         double creditPeriodMonth = creditPeriodYear * 12;
-
         double koefficient = creditRateMonth * Math.pow(1 + creditRateMonth, creditPeriodMonth) / (Math.pow(1 + creditRateMonth, creditPeriodMonth) - 1);
-
-        double payment;
-
-        payment = creditSum * koefficient;
-
+        double payment = creditSum * koefficient;
         return payment;
-
-
     }
-
 }
